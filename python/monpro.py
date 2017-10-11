1207e5958a4b9e1f7f6bca8b476a49fca99b7976
Johannes Wågen <johannes@wagen.no>
2017-10-11 22:19:54 +0200

    Functioning command line interface

--- python/monpro.py
+++ python/monpro.py
@@ -1,3 +1,6 @@
+import click
+
+
 def getBit(a, n):
     """Get the nth bit from a"""
     if (a & (1<<n)):
@@ -12,7 +15,6 @@ def monpro(a, b, n, k = 128):
     if k == None:    
         k = len(bin(max(a,b)))
     u = 0
-    print(k)
 
     for i in range(0, k):
         if getBit(a, i):
@@ -38,8 +40,8 @@ def monexp(M, e, n):
     # Convert to montgomery form
     M_ = monpro(M, r_2, n)
     x_ = monpro(1, r_2, n)
-    print("M_ = ", M_)
-    print("x_ = ", x_)
+    #print("M_ = ", M_)
+    #print("x_ = ", x_)
 
 
     for i in range(k-1, -1, -1): #Loop from msb to lsp
@@ -70,3 +72,43 @@ def testmonpro(a,b,n):
     print('X = ', x, )
     return x
 
+
+@click.command()
+@click.option('-m', type=(int), help='Message to be encrypted.')
+@click.option('--public-key', type=int, help='Public exponent.')
+@click.option('--private-key', type=int, help='Private exponent.')
+@click.option('-n', type=int, help='Modulo number.')
+@click.option('-k', type=int, default=128, help='Number of bits to use.')
+@click.option('--encrypt', is_flag=True, help='Encrypt the message.')
+@click.option('--decrypt', is_flag=True, help='Decrypt the message.')
+def cli(m, public_key, private_key, n, k, encrypt, decrypt):
+    """
+    Command line interface for montgomery exponentiation.
+    
+    Examples:
+    Loopback with both --encrypt and --decrypt enabled
+    monpro.py --encrypt --decrypt -m 12312311 --public-key 65537 --private-key 123393368995652170316939386126073244113 -n 196525583218743014100704959829497557023
+    Encrypted  12312311 , encrypted value  139608985220169980430515913107853261509 , decrypted value  12312311
+    """
+
+    if encrypt and decrypt:
+        encrypted_message = monexp(m, public_key, n)
+        decrypted = monexp(encrypted_message, private_key, n)
+        
+        print("Encrypted ", m, ", encrypted value ", encrypted_message,", decrypted value ", decrypted)
+        return
+    
+    elif encrypt:
+        encrypted_message = monexp(m, public_key, n)
+        print("Encrypted ", m, ", encrypted value ", encrypted_message)
+
+
+    elif decrypt:
+        decrypted = monexp(m, private_key, n)
+        print(", encrypted value ", m, ", decrypted value ", decrypted)
+
+
+
+
+if __name__ == '__main__':
+    cli()
