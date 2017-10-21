4d0603a05745fbd0145b1ec1c29e77c59d3cea70
Johannes Wågen <johannes@wagen.no>
2017-10-21 15:05:43 +0200

    MonPro_tb working

--- vhdl/testbench/MonPro_tb.vhd
+++ vhdl/testbench/MonPro_tb.vhd
@@ -7,29 +7,31 @@ use ieee.numeric_std.all;
 entity MonPro_tb is
 end MonPro_tb;
 
-architecture bahav of MonPro_tb is
+architecture bahavioral of MonPro_tb is
     --Clock parameters
     constant CLK_PERIOD : time := 10ns;
     constant RESET_TIME : time := 10ns;
 
-    signal clk         : in std_logic := '0';
-    signal resetn      : in std_logic := '0';
-    signal start       : in std_logic;
-    signal a           : in std_logic_vector(k downto 0);
-    signal b           : in std_logic_vector(k downto 0);
-    signal n           : in std_logic_vector(k downto 0);
-    signal done        : out std_logic;
-    signal u           : out std_logic_vector(k downto 0);
+    constant k : integer := 128;
+
+    signal clk         : std_logic := '0';
+    signal resetn      : std_logic := '0';
+    signal start       : std_logic;
+    signal a           : std_logic_vector(k downto 0);
+    signal b           : std_logic_vector(k downto 0);
+    signal n           : std_logic_vector(k downto 0);
+    signal done        : std_logic;
+    signal u           : std_logic_vector(k downto 0);
 
 begin
     -- Connect dut
-    dut : entity work.Monpro
+    dut : entity work.MonPro
         port map (
             clk         => clk,          
             resetn      => resetn,       
             start       => start,        
-            a,          => a,            
-            b,          => b,            
+            a           => a,            
+            b           => b,            
             n           => n,            
             done        => done,         
             u           => u            
@@ -47,16 +49,16 @@ begin
     end process;
 
     -- Stimuli
-    stimuli_proc: precess
+    stimuli_proc: process
     begin
-        wait for RESET_TIME;
+        wait for 1*RESET_TIME;
 
-        wait for CLK_PERIOD;
+        wait for 1*CLK_PERIOD;
 
         start <= '1';
-        wait for CLK_PERIOD;
+        wait for 1*CLK_PERIOD;
         start <= '0';
-        wait for 127*CLK_PERIOD;
+        wait for 130*CLK_PERIOD;
 
         assert done = '1'report "Done not 1 after full count" severity failure;
         
@@ -68,4 +70,4 @@ begin
 
 
 
-end architecture;
+end;
