9093e174cb39e7447880e6913e8ae599d1e7f1fe
Johannes Wågen <johannes@wagen.no>
2017-11-13 15:49:14 +0100

    Added register between stage 1 and 2 of monpro

--- vhdl/src/MonPro.vhd
+++ vhdl/src/MonPro.vhd
@@ -27,13 +27,17 @@ architecture circuit of MonPro is
   type state_type is (IDLE, LOOPING, SUBTRACTING, FINISHED);
   signal state : state_type;
 
+  type stage_type is (FIRST, SECOND);
+  signal stage : stage_type;
+
 
   -- Use k+1 bits for the intermediate registers
   -- The upper bound of the intermediate result before downshift is:
   -- (N-1)*3 = 3N - 3. This requires two extra bits to contain
   -- u_intermediate could be only k bits long, as long it only holds the
   -- result of the shift opperation. It is kept at k+1 bits for ease of use.
-  signal u_intermediate : std_logic_vector(k+1 downto 0);
+  signal u_intermediate      : std_logic_vector(k+1 downto 0);
+  signal u_intermadiates1_s2 : std_logic_vector(k+1 downto 0);
   --signal c_out_lower : std_logic;
 
   signal loop_counter : natural range 0 to k-1 := 0;
@@ -47,13 +51,14 @@ begin
     if rising_edge(clk) then
       if resetn = '0' then
         state        <= IDLE;
+        stage        <= FIRST;
         loop_counter <= 0;
 
         u              <= (others => '0');
         u_intermediate <= (others => '0');
       else
-				-- Intermediate calculation
-				-- Stage 2a
+        -- Intermediate calculation
+        -- Stage 2a
         if a(loop_counter) = '1' then
           u_temp1 := std_logic_vector(unsigned(u_intermediate) + unsigned("00" & b));
         else
@@ -61,12 +66,19 @@ begin
         end if;
 
         -- Stage 2a
-        if u_temp1(0) = '1' then
-          u_temp2 := std_logic_vector(unsigned(u_temp1) + unsigned("00" & n));
+        if u_intermadiates1_s2(0) = '1' then
+          u_temp2 := std_logic_vector(unsigned(u_intermadiates1_s2) + unsigned("00" & n));
         else
-          u_temp2 := u_temp1;
+          u_temp2 := u_intermadiates1_s2;
         end if;
 
+        --Default values
+        state        <= IDLE;
+        stage        <= FIRST;
+        loop_counter <= 0;
+
+        u              <= (others => '0');
+        u_intermediate <= (others => '0');
 
         -- State machine
         case (state) is
@@ -74,11 +86,11 @@ begin
             u <= (others => '0');
 
 
-						-- Do start first loop imediatly on start signal
+                                        -- Do start first loop imediatly on start signal
             if start = '1' then
-              state          <= LOOPING;
-              loop_counter   <= 1;
-              u_intermediate <= '0' & u_temp2(k+1 downto 1);
+              state <= LOOPING;
+            --loop_counter   <= 1;
+            --u_intermediate <= '0' & u_temp2(k+1 downto 1);
             else
               state          <= IDLE;
               loop_counter   <= 0;
@@ -87,23 +99,32 @@ begin
 
           when LOOPING =>
             u <= (others => '0');
+            if(stage = FIRST) then
+              u_intermadiates1_s2 <= u_temp1;
+              stage               <= SECOND;
+            else
+              u_intermediate <= '0' & u_temp2(k+1 downto 1);
+              stage          <= FIRST;
 
-						--Shift intermediate
-            u_intermediate <= '0' & u_temp2(k+1 downto 1);
+            end if;
 
-            if loop_counter = k-1 then
+            if loop_counter = k-1 and stage = SECOND then
               state        <= SUBTRACTING;
               loop_counter <= 0;
             else
-              state        <= LOOPING;
-              loop_counter <= loop_counter + 1;
+              state <= LOOPING;
+              if(stage = SECOND) then
+                loop_counter <= loop_counter + 1;
+              else
+                loop_counter <= loop_counter;
+              end if;
             end if;
 
           when SUBTRACTING =>
             loop_counter   <= 0;
             u_intermediate <= (others => '0');
 
-						-- Subtraction has to be don with the k+1 bits to be correct
+                                        -- Subtraction has to be don with the k+1 bits to be correct
             if u_intermediate >= ("00" & n) then
               u_next_temp := std_logic_vector(unsigned(u_intermediate) - unsigned("00" & n));
               u           <= u_next_temp(k-1 downto 0);
@@ -116,12 +137,12 @@ begin
           when FINISHED =>
             u <= (others => '0');
 
-						-- Jump to looping if the start signal already is pressent
-						-- Do start first loop imediatly on start signal
+                                        -- Jump to looping if the start signal already is pressent
+                                        -- Do start first loop imediatly on start signal
             if start = '1' then
-              state          <= LOOPING;
-              loop_counter   <= 1;
-              u_intermediate <= '0' & u_temp2(k+1 downto 1);
+              state <= LOOPING;
+            --loop_counter   <= 1;
+            --u_intermediate <= '0' & u_temp2(k+1 downto 1);
             else
               state          <= IDLE;
               loop_counter   <= 0;
